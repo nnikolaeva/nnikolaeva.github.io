705ec298949fc46a9041aa1ac722e9fd53c453e3
User <noemail@nomail.no>
2016-04-12 16:50:05 -0700

    Refactoring

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-    var width = $('.justified').width();
-    $('.justified').css('margin-left', '-' + (width / 2) + 'px');
-
     $(".nav a").on("click", function() {
         $(".nav").find(".active").removeClass("active");
         $(this).parent().addClass("active");
