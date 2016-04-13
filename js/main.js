2ebecbaab9c9f4d3c3dddadbe9e5c0a90d900899
User <noemail@nomail.no>
2016-04-13 10:13:16 -0700

    Add the Scrollspy Plugin

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
-    $(".nav a").on("click", function() {
-        $(".nav").find(".active").removeClass("active");
-        $(this).parent().addClass("active");
-    });
+// Add smooth scrolling to all links inside a navbar
+$("#main-menu a").on('click', function(event){
+
+  // Prevent default anchor click behavior
+  event.preventDefault();
+
+  // Store hash (#)
+  var hash = this.hash;
+
+  // Using jQuery's animate() method to add smooth page scroll
+  // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area (the speed of the animation)
+  $('html, body').animate({
+    scrollTop: $(hash).offset().top
+  }, 800, function(){
+
+    // Add hash (#) to URL when done scrolling (default click behavior)
+    window.location.hash = hash;
+  });
+});    
 });
